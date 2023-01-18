6011d36f654744d17d82b8a7fa8689d03dd9aa1f
Simona Mazzarino <simona@clearbox.ai>
2023-01-18 16:36:12 +0100

    Issue #1: order ner function and add method for numeric columns

--- ner.py
+++ ner.py
@@ -23,39 +23,50 @@ def ner(df_input_path): #df_input_path: string containing path of csv file
     # Presidio was created as a tool to recognize entities in text, the following line is used to analyze a pandas dataframe
     # by first converting it to json
     analyzer_results = list(batch_analyzer.analyze_dict(df_input.to_dict(orient="list"), language="en"))
+
+    # dict_entities is a dictionary where the columns' names are the keys and the entity and the confidence are the values
+    dict_global_entities = {}
+    listOf_df_cols = [col for col in df_input.columns]
+
+    for col in listOf_df_cols:
+        dict_global_entities[col] = None
     
-    # the following part creates a python dictionary (keys-> column names, values-> entity type)
-    l = {}
+    for col in listOf_df_cols:
+    #assigning ZIPCODE entity
+        if (('postal' in col.lower()) and ('code' in col.lower())) or (('zip' in col.lower()) and (
+            'code' in col.lower())) or (('zip' in col.lower())):
+            dict_global_entities[col] = ['ZIPCODE', 1.0]
+    #assigning CREDIT_CARD_NUMBER entity
+        elif (('credit' in col.lower()) or ('card' in col.lower())) and ('number' in col.lower()):
+            dict_global_entities[col] = ['CREDIT_CARD_NUMBER', 1.0]
+    
+    # the following part creates a python dictionary (keys-> column names, values-> entity type) for columns of type object
+    dict_typeObject_entities = {}
     for w, j in zip(df_input.columns, analyzer_results):
         if df_input[w].dtype == 'object':
-            l[w] = [i[0].entity_type for i in j.recognizer_results if len(i) > 0]
-  
-    list_entities = {}
-    cols = [col for col in df_input.columns]
-
-    for col in cols:
-        list_entities[col] = None
+            dict_typeObject_entities[w] = [i[0].entity_type for i in j.recognizer_results if len(i) > 0]
         
-    cols = [col for col in l.keys() if len(l[col]) > 0.1 * df_input.shape[0]]
+    cols = [col for col in dict_typeObject_entities.keys() if len(dict_typeObject_entities[col]) > 0.1 * df_input.shape[0]]
     
     # this is a very heuristic loop that assigns each object column with an entity
   
     for col in cols:
-        lst = [i for i in l[col]]
+        lst = [i for i in dict_typeObject_entities[col]]
         #assigning LOCATION entity
         if ('LOCATION' in lst) and ('name' not in col.lower()) and (
-                len([i for i in l[col] if i == 'LOCATION']) > 0.1 * df_input.shape[0]):
-            list_entities[col] = ['LOCATION', len([i for i in l[col] if i == 'LOCATION'])/df_input.shape[0]]
+                len([i for i in dict_typeObject_entities[col] if i == 'LOCATION']) > 0.1 * df_input.shape[0]):
+            dict_global_entities[col] = ['LOCATION', len([i for i in dict_typeObject_entities[col] 
+            if i == 'LOCATION'])/df_input.shape[0]]
         #assigning ZIPCODE entity if zipcode in dataset is 'object' type
         elif (('postal' in col.lower()) and ('code' in col.lower())) or (('zip' in col.lower()) and (
                 'code' in col.lower())) or (('zip' in col.lower())): 
-            list_entities[col] = ['ZIPCODE', 1.0]
+            dict_global_entities[col] = ['ZIPCODE', 1.0]
         #assigning CREDIT_CARD_NUMBER entity if credit card number in dataset is 'object' type
         elif ((('credit' in col.lower()) or ('card' in col.lower())) and ('number' in col.lower())):
-            list_entities[col] = ['CREDIT_CARD_NUMBER', 1.0]
+            dict_global_entities[col] = ['CREDIT_CARD_NUMBER', 1.0]
         else:
             most_freq = max(set(lst), key=lst.count)
-            list_entities[col] = [most_freq, len([i for i in lst if i == most_freq])/df_input.shape[0]]
+            dict_global_entities[col] = [most_freq, len([i for i in lst if i == most_freq])/df_input.shape[0]]
 
     
-    return list_entities
+    return dict_global_entities
